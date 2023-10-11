e0edd3f478b3689a083079582d56b1a7833f79d1
Vera Lisitsa <nvk1984@mail.ru>
2023-10-11 22:46:17 +0300

    Add ApplicationTests

--- src/test/java/com/example/springboot/ApplicationTests.java
+++ src/test/java/com/example/springboot/ApplicationTests.java
@@ -1,13 +1,44 @@
 package com.example.springboot;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
+    @Container
+    private static final GenericContainer<?> devApp = new GenericContainer<>("devapp")
+            .withExposedPorts(8080);
+    @Container
+    private static final GenericContainer<?> prodApp = new GenericContainer<>("prodapp")
+            .withExposedPorts(8081);
+    @Autowired
+    TestRestTemplate restTemplate;
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    public static void setUp() {
+        devApp.start();
+        prodApp.start();
+    }
+
+    @Test
+    void contextLoadsDevApp() {
+        String result = "Current profile is dev";
+        ResponseEntity<String> entityForDevApp = restTemplate.getForEntity("http://localhost:" + devApp.getMappedPort(8080) + "/profile", String.class);
+        Assertions.assertEquals(result, entityForDevApp.getBody());
+    }
+
+    @Test
+    void contextLoadsProdApp() {
+        String result = "Current profile is production";
+        ResponseEntity<String> entityForProdApp = restTemplate.getForEntity("http://localhost:" + prodApp.getMappedPort(8081) + "/profile", String.class);
+        Assertions.assertEquals(result, entityForProdApp.getBody());
+    }
 
 }
